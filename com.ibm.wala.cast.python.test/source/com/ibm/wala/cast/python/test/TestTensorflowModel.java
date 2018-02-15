9bdaab1afdbb2397e4636407d81d28aff023f01b
Julian Dolby <juliandolby@gmail.com>
2018-02-14 22:06:18 -0500

    a little more testing

--- com.ibm.wala.cast.python.test/source/com/ibm/wala/cast/python/test/TestTensorflowModel.java
+++ com.ibm.wala.cast.python.test/source/com/ibm/wala/cast/python/test/TestTensorflowModel.java
@@ -1,17 +1,36 @@
 package com.ibm.wala.cast.python.test;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 import org.junit.Test;
 
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.CallGraph;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
 import com.ibm.wala.util.CancelException;
 
 public class TestTensorflowModel extends TestPythonCallGraphShape {
 
 	@Test
-	public void testPf1() throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
-		System.err.println(process("tf1.py").fst);
+	public void testTf1() throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
+		CallGraph CG = process("tf1.py").fst;
+		System.err.println(CG);
+		
+		Collection<CGNode> nodes = getNodes(CG, "Lmodel_fn");
+		assert ! nodes.isEmpty();
+		check: {
+			for(CGNode node : nodes) {
+				for(Iterator<CGNode> ns = CG.getPredNodes(node); ns.hasNext(); ) {
+					if (ns.next().getMethod().isSynthetic()) {
+						break check;
+					}
+				}
+			
+				assert false : node + " should have synthetic caller";
+			}
+		}
 	}
 
 }
