67c593b401193baffe5372d9b91310e28013fded
Julian Dolby <juliandolby@gmail.com>
2018-09-29 12:24:00 -0400

    add missing script

---
+++ com.ibm.wala.cast.python.ml/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ME=`realpath $0`
+DIR=`dirname $ME`
+
+cat -u | tee -a /tmp/lsp.in.log | $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:6660,server=y,suspend=n -jar $DIR/target/com.ibm.wala.cast.python.ml-0.0.1-SNAPSHOT.jar --mode stdio | tee -a /tmp/lsp.out.log
