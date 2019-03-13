45343a050abe1424c2b1c826499c619832b7575d
Julian Dolby <juliandolby@gmail.com>
2019-03-13 18:15:51 -0400

    error handling

---
+++ com.ibm.wala.cast.python/source/org/python/antlr/WalaErrorHandler.java
@@ -0,0 +1,76 @@
+package org.python.antlr;
+
+import org.python.antlr.base.expr;
+import org.python.antlr.base.mod;
+import org.python.antlr.base.slice;
+import org.python.antlr.base.stmt;
+import org.python.antlr.runtime.BaseRecognizer;
+import org.python.antlr.runtime.BitSet;
+import org.python.antlr.runtime.IntStream;
+import org.python.antlr.runtime.Lexer;
+import org.python.antlr.runtime.RecognitionException;
+
+import com.ibm.wala.cast.python.parser.WalaPythonParser;
+
+public class WalaErrorHandler implements ErrorHandler {
+	private final ErrorHandler base;
+	private final WalaPythonParser parser;
+	
+	public WalaErrorHandler(ErrorHandler base, WalaPythonParser parser) {
+		this.base = base;
+		this.parser = parser;
+	}
+
+	@Override
+	public void reportError(BaseRecognizer br, RecognitionException re) {
+		parser.recordError(re);
+		base.reportError(br, re);
+	}
+
+	@Override
+	public void recover(BaseRecognizer br, IntStream input, RecognitionException re) {
+		base.recover(br, input, re);
+	}
+
+	@Override
+	public void recover(Lexer lex, RecognitionException re) {
+		base.recover(lex, re);
+	}
+
+	@Override
+	public boolean mismatch(BaseRecognizer br, IntStream input, int ttype, BitSet follow) throws RecognitionException {
+		return base.mismatch(br, input, ttype, follow);
+	}
+
+	@Override
+	public Object recoverFromMismatchedToken(BaseRecognizer br, IntStream input, int ttype, BitSet follow)
+			throws RecognitionException {
+		return base.recoverFromMismatchedToken(br, input, ttype, follow);
+	}
+
+	@Override
+	public expr errorExpr(PythonTree t) {
+		return base.errorExpr(t);
+	}
+
+	@Override
+	public mod errorMod(PythonTree t) {
+		return base.errorMod(t);
+	}
+
+	@Override
+	public slice errorSlice(PythonTree t) {
+		return base.errorSlice(t);
+	}
+
+	@Override
+	public stmt errorStmt(PythonTree t) {
+		return base.errorStmt(t);
+	}
+
+	@Override
+	public void error(String message, PythonTree t) {
+		base.error(message, t);
+	}
+
+}
