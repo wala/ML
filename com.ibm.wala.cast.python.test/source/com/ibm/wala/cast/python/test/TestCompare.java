8d8e51cdd2eea1a3639adb6bca6c634c33cd1434
Julian Dolby <juliandolby@gmail.com>
2019-09-19 13:47:13 -0400

    tests

--- com.ibm.wala.cast.python.test/source/com/ibm/wala/cast/python/test/TestCompare.java
+++ com.ibm.wala.cast.python.test/source/com/ibm/wala/cast/python/test/TestCompare.java
@@ -1,32 +1,83 @@
 package com.ibm.wala.cast.python.test;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Consumer;
 
 import org.junit.Test;
 
+import com.ibm.wala.cast.python.client.PythonAnalysisEngine;
+import com.ibm.wala.cfg.cdg.ControlDependenceGraph;
+import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.ipa.callgraph.propagation.PropagationCallGraphBuilder;
 import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.ssa.ISSABasicBlock;
+import com.ibm.wala.ssa.SSACFG;
+import com.ibm.wala.ssa.SSAReturnInstruction;
 import com.ibm.wala.util.CancelException;
 
-public class TestAssign extends TestPythonCallGraphShape {
+public class TestCompare extends TestPythonCallGraphShape {
 
-	 protected static final Object[][] assertionsAssign1 = new Object[][] {
-		    new Object[] { ROOT, new String[] { "script assign1.py" } },
+	 protected static final Object[][] assertionsCmp1 = new Object[][] {
+		    new Object[] { ROOT, new String[] { "script cmp1.py" } },
 		    new Object[] {
-		        "script assign1.py",
-		        new String[] { "script assign1.py/f", "script assign1.py/g" } },
-		    new Object[] {
-			        "script assign1.py/f",
-			        new String[] { "script assign1.py/a" } },
-		    new Object[] {
-			        "script assign1.py/g",
-			        new String[] { "script assign1.py/a" } }
+		        "script cmp1.py",
+		        new String[] { "script cmp1.py/ctwo", "script cmp1.py/cthree", "script cmp1.py/cfour" } }
 	 };
 
+	 private void findReturns(CGNode n, Consumer<SSAReturnInstruction> act) {
+		 n.getIR().iterateNormalInstructions().forEachRemaining(inst -> { 
+			 if (inst instanceof SSAReturnInstruction) {
+				 act.accept((SSAReturnInstruction)inst);
+			 }
+		 });
+	 }
+	 
 	@Test
 	public void testAssign1() throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
-		CallGraph CG = process("assign1.py");
-		verifyGraphAssertions(CG, assertionsAssign1);
+		PythonAnalysisEngine<?> e = new PythonAnalysisEngine<Void>() {
+			@Override
+			public Void performAnalysis(PropagationCallGraphBuilder builder) throws CancelException {
+				assert false;
+				return null;
+			}
+		};
+		e.setModuleFiles(Collections.singleton(getScript("cmp1.py")));
+		PropagationCallGraphBuilder cgBuilder = (PropagationCallGraphBuilder) e.defaultCallGraphBuilder();
+		CallGraph CG = cgBuilder.makeCallGraph(cgBuilder.getOptions());
+		verifyGraphAssertions(CG, assertionsCmp1);
+		
+		
+		Collection<CGNode> ctwo = this.getNodes(CG, "script cmp1.py/ctwo");
+		assert ! ctwo.isEmpty();
+		check(ctwo);
+		
+		Collection<CGNode> cthree = this.getNodes(CG, "script cmp1.py/cthree");
+		assert ! cthree.isEmpty();
+		check(cthree);
+
+		Collection<CGNode> cfour = this.getNodes(CG, "script cmp1.py/cfour");
+		assert ! cfour.isEmpty();
+		check(cfour);
+
+	}
+
+	private void check(Collection<CGNode> ctwo) {
+		ctwo.forEach(n -> { 
+			findReturns(n, inst -> {
+				SSACFG cfg = n.getIR().getControlFlowGraph();
+				ControlDependenceGraph<ISSABasicBlock> cdg = new ControlDependenceGraph<>(cfg, true);
+				
+				ISSABasicBlock bb = cfg.getBlockForInstruction(inst.iIndex());
+				while(cdg.getPredNodeCount(bb) > 0) {
+					ISSABasicBlock pb = cdg.getPredNodes(bb).next();
+					System.err.println(bb);
+					bb = pb;
+				}
+			});
+		});
 	}
 
 }
