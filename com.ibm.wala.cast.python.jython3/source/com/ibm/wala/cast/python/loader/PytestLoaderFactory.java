f4abb0c027466df59fcf233b747633c80be87c36
Julian Dolby <dolby@us.ibm.com>
2021-02-15 12:12:10 -0500

    start of pytest support

---
+++ com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/loader/PytestLoaderFactory.java
@@ -0,0 +1,13 @@
+package com.ibm.wala.cast.python.loader;
+
+import com.ibm.wala.classLoader.IClassLoader;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
+
+public class PytestLoaderFactory extends PythonLoaderFactory {
+
+	@Override
+	protected IClassLoader makeTheLoader(IClassHierarchy cha) {
+		return new PytestLoader(cha);
+	}
+
+}
