547c20a3f8044f00492411f4a64fe5c74b3bc2ac
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-04-15 15:29:49 -0400

    Address https://github.com/wala/ML/issues/28.
    See https://maven.apache.org/plugins/maven-shade-plugin/examples/attached-artifact.html for reference.

--- com.ibm.wala.cast.python.ml/run.sh
+++ com.ibm.wala.cast.python.ml/run.sh
@@ -3,4 +3,4 @@
 ME=`realpath $0`
 DIR=`dirname $ME`
 
-cat -u | tee -a /tmp/lsp.in.log | $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:6660,server=y,suspend=n -jar $DIR/target/com.ibm.wala.cast.python.ml-0.0.1-SNAPSHOT.jar --mode stdio | tee -a /tmp/lsp.out.log
+cat -u | tee -a /tmp/lsp.in.log | $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:6660,server=y,suspend=n -jar $DIR/target/com.ibm.wala.cast.python.ml-0.0.1-SNAPSHOT-shaded.jar --mode stdio | tee -a /tmp/lsp.out.log
