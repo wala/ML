a705dee2497760f1fc887e6dfb759847854a63ef
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-13 13:50:59 -0400

    Add more (including multi-level" TF2 APIs (#47)
    The added code can process more TF2 APIs (`tf.Variable`, `tf.zeros`, and
    `tf.constant`), including a multi-level TF API `random.uniform`.

--- com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
+++ com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
@@ -61,6 +61,10 @@ public class TestTensorflowModel extends TestPythonMLCallGraphShape {
 		testTf2("tf2.py", "add", 2, 2, 3);
 		testTf2("tf2b.py", "add", 2, 2, 3);
 		testTf2("tf2c.py", "add", 2, 2, 3);
+		testTf2("tf2d.py", "add", 2, 2, 3);
+		testTf2("tf2e.py", "add", 2, 2, 3);
+		testTf2("tf2f.py", "add", 2, 2, 3);
+		testTf2("tf2g.py", "add", 2, 2, 3);
 	}
 
 	private void testTf2(String filename, String functionName, int expectedNumberOfTensorParameters, int... expectedValueNumbers) throws ClassHierarchyException, CancelException, IOException {
