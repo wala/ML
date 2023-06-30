f5540e97f4b89d5b8fb95cdce396a0415fe52e89
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-30 16:32:48 -0400

    Adding second level APIs

---
+++ com.ibm.wala.cast.python.test/data/tf2bb4.py
@@ -0,0 +1,10 @@
+from tensorflow.python.ops.ragged.ragged_tensor import RaggedTensor
+import tensorflow as tf
+
+def add(a, b):
+  return a + b
+
+
+nested_value_rowids = [tf.constant([0, 0, 1, 3, 3], tf.int64),tf.constant([0, 0, 2, 2, 2, 3, 4], tf.int64)]
+x = tf.keras.Input(shape=[None], dtype=tf.string)
+c = add(tf.RaggedTensor.from_nested_value_rowids(x, nested_value_rowids),RaggedTensor.from_nested_value_rowids(x, nested_value_rowids))
---
+++ com.ibm.wala.cast.python.test/data/tf2hh.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+def add(a, b):
+  return a + b
+
+
+c = add(tf.random.gamma([10], [0.5, 1.5]), tf.random.gamma([10], [1, 2.5]))
