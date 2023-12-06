af99c339730fae38634d2b1455a2cf57902022d1
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-12-05 21:46:47 -0500

    Issue 106 workaround (#62)
    Workaround issue #106.

--- com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
+++ com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
@@ -209,12 +209,7 @@ public class TestTensorflowModel extends TestPythonMLCallGraphShape {
     testTf2("tf2_test_tensor_list2.py", "add", 0, 2);
     testTf2("tf2_test_tensor_list3.py", "add", 0, 2);
     testTf2("tf2_test_model_call.py", "SequentialModel.__call__", 1, 4, 3);
-    testTf2(
-        "tf2_test_model_call2.py",
-        "SequentialModel.call",
-        0,
-        2); // NOTE: Change to testTf2("tf2_test_model_call2.py", "SequentialModel.call", 1, 4, 3)
-    // once https://github.com/wala/ML/issues/106 is fixed.
+    testTf2("tf2_test_model_call2.py", "SequentialModel.call", 1, 4, 3);
     testTf2("tf2_test_model_call3.py", "SequentialModel.call", 1, 4, 3);
     testTf2("tf2_test_model_call4.py", "SequentialModel.__call__", 1, 4, 3);
     testTf2("tf2_test_callbacks.py", "replica_fn", 1, 3, 2);
