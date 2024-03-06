b6a6ce2657690610c56e3e58e0d3c5cfebe169e9
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-03-06 13:14:08 -0500

    Apply spotless.

--- com.ibm.wala.cast.python/source/com/ibm/wala/cast/python/loader/PythonLoader.java
+++ com.ibm.wala.cast.python/source/com/ibm/wala/cast/python/loader/PythonLoader.java
@@ -205,7 +205,7 @@ public abstract class PythonLoader extends CAstAbstractModuleLoader {
   final CoreClass superfun =
       new CoreClass(PythonTypes.superfun.getName(), PythonTypes.CodeBody.getName(), this, null);
   final CoreClass iterator =
-	  new CoreClass(PythonTypes.iterator.getName(), PythonTypes.object.getName(), this, null);
+      new CoreClass(PythonTypes.iterator.getName(), PythonTypes.object.getName(), this, null);
 
   public PythonLoader(IClassHierarchy cha, IClassLoader parent) {
     super(cha, parent);
