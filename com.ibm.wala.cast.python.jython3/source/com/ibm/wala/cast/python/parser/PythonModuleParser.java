6a8e7183bf1f572adac685374063be14c8a82fbb
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-03-26 10:36:06 -0400

    Remove copy and paste error.

--- com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/parser/PythonModuleParser.java
+++ com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/parser/PythonModuleParser.java
@@ -1,4 +1,4 @@
-/, "500", "1000", "5000", "10000"******************************************************************************
+/******************************************************************************
  * Copyright (c) 2018 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
