aa8b74394ce44b81a5d8402a0c0c44bdafb95195
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-03-27 15:04:33 -0400

    Use the filename if it's a source file module.
    We have the full file path, why not use it? I see in 3cf9b356211c282571363a59b8c08b2844d70f5b that this was changed for `FileModule`s, but I cannot understand why.

--- com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/parser/PythonModuleParser.java
+++ com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/parser/PythonModuleParser.java
@@ -19,6 +19,7 @@ import com.ibm.wala.cast.util.CAstPrinter;
 import com.ibm.wala.classLoader.FileModule;
 import com.ibm.wala.classLoader.Module;
 import com.ibm.wala.classLoader.ModuleEntry;
+import com.ibm.wala.classLoader.SourceFileModule;
 import com.ibm.wala.classLoader.SourceModule;
 import com.ibm.wala.classLoader.SourceURLModule;
 import com.ibm.wala.util.collections.HashSetFactory;
@@ -142,7 +143,8 @@ public class PythonModuleParser extends PythonParser<ModuleEntry> {
   }
 
   private static String scriptName(SourceModule fileName) {
-    if (fileName instanceof FileModule) {
+    if (fileName instanceof SourceFileModule) return fileName.getName();
+    else if (fileName instanceof FileModule) {
       return fileName.getClassName();
     } else {
       return fileName.getName();
