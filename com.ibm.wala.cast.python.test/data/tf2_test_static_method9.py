c89c9aca5a4cb3b3129e3811268d51beca4caf5d
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-04-25 17:13:57 -0400

    Format Python code.

--- com.ibm.wala.cast.python.test/data/tf2_test_static_method9.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_static_method9.py
@@ -3,11 +3,11 @@ import tensorflow as tf
 
 class MyClass:
 
-  @staticmethod
-  @tf.function
-  def the_static_method(x, y):
-    assert isinstance(x, tf.Tensor)
-    assert isinstance(y, tf.Tensor)
+    @staticmethod
+    @tf.function
+    def the_static_method(x, y):
+        assert isinstance(x, tf.Tensor)
+        assert isinstance(y, tf.Tensor)
 
 
 MyClass().the_static_method(tf.constant(1), tf.constant(2))
