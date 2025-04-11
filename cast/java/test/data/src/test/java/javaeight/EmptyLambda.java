317c06a3d941a8c8945ad0de9bbe1ee975a61ba4
Patrick Apgar <Patrick.Apgar@ibm.com>
2025-04-11 12:34:49 -0700

    Adding EmptyLambda file

--- cast/java/test/data/src/test/java/javaeight/EmptyLambda.java
+++ cast/java/test/data/src/test/java/javaeight/EmptyLambda.java
@@ -2,6 +2,7 @@ package javaeight;
 
 import java.util.function.Function;
 
+/* Class hook to test lambda functionality */
 public class EmptyLambda {
     public static void main(String[] args) {
         
