be241ef7665eb322f259639d83cc0892ed2a876c
Julian Dolby <dolby@us.ibm.com>
2025-05-25 18:47:43 -0400

    more interprocedural code

--- com.ibm.wala.cast.python.cpython/test/com/ibm/wala/cast/python/cpython/test/TestRaise.java
+++ com.ibm.wala.cast.python.cpython/test/com/ibm/wala/cast/python/cpython/test/TestRaise.java
@@ -60,4 +60,64 @@ public class TestRaise extends TestJythonCallGraphShape {
 
 	    verifyGraphAssertions(CG, assertionsForRaise);
 	}
+	  protected static final Object[][] assertionsForRaise2 =
+		      new Object[][] {
+		        new Object[] {ROOT, new String[] {"script raise2.py"}},
+		        new Object[] {
+		          "script raise2.py",
+		          new String[] {
+		            "script raise2.py/f1",
+		            "script raise2.py/f2",
+		            "script raise2.py/f3",
+				    "$script raise2.py/e1/f:trampoline1"}
+		        },
+		        new Object[] {
+				  "script raise2.py/f1",
+				  new String[] {
+				    "script raise2.py/e1"}
+				},
+				new Object[] {
+				  "script raise2.py/f2",
+				  new String[] {
+				    "script raise2.py/e2",
+				    "script raise2.py/e3"}
+				},
+				new Object[] {
+				  "script raise2.py/f3",
+				  new String[] {
+				    "$script raise2.py/e2/f:trampoline1",
+				  	"$script raise2.py/e3/f:trampoline1"}
+		        },
+		        new Object[] {
+		          "$script raise2.py/e1/f:trampoline1",
+		          new String[] {
+		            "script raise2.py/e1/f"}
+		        },
+		        new Object[] {
+		          "$script raise2.py/e2/f:trampoline1",
+		          new String[] {
+		            "script raise2.py/e2/f"}
+		        },
+		        new Object[] {
+		          "$script raise2.py/e3/f:trampoline1",
+		          new String[] {
+		            "script raise2.py/e3/f"}
+		        }
+	  };
+
+	@Test
+	public void testRaise2() throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
+		  PythonAnalysisEngine<?> engine = this.makeEngine("raise2.py");
+		  PropagationCallGraphBuilder callGraphBuilder = engine.defaultCallGraphBuilder();
+		  CallGraph CG = callGraphBuilder.makeCallGraph(callGraphBuilder.getOptions());
+
+		  System.err.println(CG);
+		  CAstCallGraphUtil.AVOID_DUMP.set(false);
+		  CAstCallGraphUtil.dumpCG(
+				  (SSAContextInterpreter) callGraphBuilder.getContextInterpreter(),
+				  callGraphBuilder.getPointerAnalysis(),
+				  CG);
+
+	    verifyGraphAssertions(CG, assertionsForRaise2);
+	}
 }
