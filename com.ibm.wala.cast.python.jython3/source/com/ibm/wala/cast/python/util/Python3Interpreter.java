f4a732518b66d83ca1973f1bb0db3a633c9bf2f9
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-07-21 12:56:00 -0400

    Fix https://github.com/wala/ML/issues/289.

--- com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/util/Python3Interpreter.java
+++ com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/util/Python3Interpreter.java
@@ -1,5 +1,7 @@
 package com.ibm.wala.cast.python.util;
 
+import org.python.core.PyObject;
+import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 
 public class Python3Interpreter extends com.ibm.wala.cast.python.util.PythonInterpreter {
@@ -8,24 +10,17 @@ public class Python3Interpreter extends com.ibm.wala.cast.python.util.PythonInte
 
   public static PythonInterpreter getInterp() {
     if (interp == null) {
-      //			PySystemState.initialize(  );
+      PySystemState.initialize();
       interp = new PythonInterpreter();
     }
     return interp;
   }
 
   public Integer evalAsInteger(String expr) {
-    /*
     PyObject val = getInterp().eval(expr);
     if (val.isInteger()) {
-    	return val.asInt();
+      return val.asInt();
     } else {
-    	return null;
-    }
-    */
-    try {
-      return Integer.parseInt(expr);
-    } catch (NumberFormatException e) {
       return null;
     }
   }
