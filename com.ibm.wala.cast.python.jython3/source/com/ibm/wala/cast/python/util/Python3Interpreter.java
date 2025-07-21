481948d0193dd090cb766235bf485e78f7ece22b
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-07-21 16:37:20 -0400

    Make edits according to Copilot review.

--- com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/util/Python3Interpreter.java
+++ com.ibm.wala.cast.python.jython3/source/com/ibm/wala/cast/python/util/Python3Interpreter.java
@@ -1,11 +1,15 @@
 package com.ibm.wala.cast.python.util;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.python.core.PyException;
 import org.python.core.PyObject;
 import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 
 public class Python3Interpreter extends com.ibm.wala.cast.python.util.PythonInterpreter {
+
+  private static final Logger LOGGER = Logger.getLogger(Python3Interpreter.class.getName());
 
   private static PythonInterpreter interp;
 
@@ -24,7 +28,8 @@ public class Python3Interpreter extends com.ibm.wala.cast.python.util.PythonInte
         return val.asInt();
       }
     } catch (PyException e) {
-
+      LOGGER.log(Level.SEVERE, "Unable to interpret Python expression: " + expr, e);
+      throw new IllegalArgumentException("Can't interpret Python expression: " + expr + ".", e);
     }
 
     return null;
