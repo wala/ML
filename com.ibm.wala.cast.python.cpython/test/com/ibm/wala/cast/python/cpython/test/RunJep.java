04051d14a422aafc2e203e8bb1e6dcb1cb821b6b
Julian Dolby <dolby@us.ibm.com>
2025-08-05 15:01:05 -0400

    jep test

---
+++ com.ibm.wala.cast.python.cpython/test/com/ibm/wala/cast/python/cpython/test/RunJep.java
@@ -0,0 +1,14 @@
+package com.ibm.wala.cast.python.cpython.test;
+
+import org.junit.Test;
+
+import com.ibm.wala.cast.python.jep.Util;
+
+public class RunJep {
+
+	@Test
+	public void runJep() {
+		assert Util.getAST("1 + a") != null;
+	}
+	
+}
